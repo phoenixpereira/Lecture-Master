4d10591c0f8987011accc47dc5b08c10bbd59686
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-07-20 17:40:10 +0930

    feat: Transcribes recorded audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
+import { AudioManager } from "./components/AudioManager";
+import Transcript from "./components/Transcript";
+import { useTranscriber } from "./hooks/useTranscriber";
+
 export default function App() {
-    return (
-        <div className='flex justify-center items-center min-h-screen'>
-            <div className='container flex flex-col justify-center items-center'>
-                <h1 className='text-3xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
-                    Lecture Master
-                </h1>
-                <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
-                    Makes watching lectures more efficient!
-                </h2>
-            </div>
+	const transcriber = useTranscriber();
 
-            <div className='absolute bottom-4'>
-                Developed by{" "}
-                <a
-                    className='underline'
-                    href='https://github.com/phoenixpereira'
-                    target="blank"
-                >
-                    Phoenix Pereira
-                </a>
-            </div>
+	return (
+		<div className='flex justify-center items-center min-h-screen'>
+			<div className='container flex flex-col justify-center items-center'>
+				<h1 className='text-3xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
+					Lecture Master
+				</h1>
+				<h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
+					Makes watching lectures more efficient!
+				</h2>
+				<AudioManager transcriber={transcriber} />
+				<Transcript transcribedData={transcriber.output} />
+			</div>
 
-        </div>
-    );
+			<div className='absolute bottom-4'>
+				Developed by{" "}
+				<a
+					className='underline'
+					href='https://github.com/phoenixpereira'
+					target="blank"
+				>
+					Phoenix Pereira
+				</a>
+			</div>
+
+		</div>
+	);
 }
---
+++ src/components/AudioManager.tsx
@@ -0,0 +1,576 @@
+import React, { useState } from "react";
+import Modal from "./modal/Modal";
+import { TranscribeButton } from "./TranscribeButton";
+import Constants from "../utils/Constants";
+import { Transcriber } from "../hooks/useTranscriber";
+
+function titleCase(str: string) {
+	str = str.toLowerCase();
+	return (str.match(/\w+.?/g) || [])
+		.map((word) => {
+			return word.charAt(0).toUpperCase() + word.slice(1);
+		})
+		.join("");
+}
+
+// List of supported languages:
+// https://help.openai.com/en/articles/7031512-whisper-api-faq
+// https://github.com/openai/whisper/blob/248b6cb124225dd263bb9bd32d060b6517e067f8/whisper/tokenizer.py#L79
+const LANGUAGES = {
+	en: "english",
+	zh: "chinese",
+	de: "german",
+	es: "spanish/castilian",
+	ru: "russian",
+	ko: "korean",
+	fr: "french",
+	ja: "japanese",
+	pt: "portuguese",
+	tr: "turkish",
+	pl: "polish",
+	ca: "catalan/valencian",
+	nl: "dutch/flemish",
+	ar: "arabic",
+	sv: "swedish",
+	it: "italian",
+	id: "indonesian",
+	hi: "hindi",
+	fi: "finnish",
+	vi: "vietnamese",
+	he: "hebrew",
+	uk: "ukrainian",
+	el: "greek",
+	ms: "malay",
+	cs: "czech",
+	ro: "romanian/moldavian/moldovan",
+	da: "danish",
+	hu: "hungarian",
+	ta: "tamil",
+	no: "norwegian",
+	th: "thai",
+	ur: "urdu",
+	hr: "croatian",
+	bg: "bulgarian",
+	lt: "lithuanian",
+	la: "latin",
+	mi: "maori",
+	ml: "malayalam",
+	cy: "welsh",
+	sk: "slovak",
+	te: "telugu",
+	fa: "persian",
+	lv: "latvian",
+	bn: "bengali",
+	sr: "serbian",
+	az: "azerbaijani",
+	sl: "slovenian",
+	kn: "kannada",
+	et: "estonian",
+	mk: "macedonian",
+	br: "breton",
+	eu: "basque",
+	is: "icelandic",
+	hy: "armenian",
+	ne: "nepali",
+	mn: "mongolian",
+	bs: "bosnian",
+	kk: "kazakh",
+	sq: "albanian",
+	sw: "swahili",
+	gl: "galician",
+	mr: "marathi",
+	pa: "punjabi/panjabi",
+	si: "sinhala/sinhalese",
+	km: "khmer",
+	sn: "shona",
+	yo: "yoruba",
+	so: "somali",
+	af: "afrikaans",
+	oc: "occitan",
+	ka: "georgian",
+	be: "belarusian",
+	tg: "tajik",
+	sd: "sindhi",
+	gu: "gujarati",
+	am: "amharic",
+	yi: "yiddish",
+	lo: "lao",
+	uz: "uzbek",
+	fo: "faroese",
+	ht: "haitian creole/haitian",
+	ps: "pashto/pushto",
+	tk: "turkmen",
+	nn: "nynorsk",
+	mt: "maltese",
+	sa: "sanskrit",
+	lb: "luxembourgish/letzeburgesch",
+	my: "myanmar/burmese",
+	bo: "tibetan",
+	tl: "tagalog",
+	mg: "malagasy",
+	as: "assamese",
+	tt: "tatar",
+	haw: "hawaiian",
+	ln: "lingala",
+	ha: "hausa",
+	ba: "bashkir",
+	jw: "javanese",
+	su: "sundanese",
+};
+
+export enum AudioSource {
+	FILE = "FILE",
+	RECORDING = "RECORDING",
+	LIVE_STREAM = "LIVE_STREAM"
+}
+
+export function AudioManager(props: { transcriber: Transcriber }) {
+	const [audioData, setAudioData] = useState<
+		| {
+			buffer: AudioBuffer;
+			url: string;
+			source: AudioSource;
+			mimeType: string;
+		}
+		| undefined
+	>(undefined);
+
+	return (
+		<>
+			<div className=' m-8 flex flex-col justify-center items-center rounded-lg bg-white shadow-xl shadow-black/5 ring-1 ring-slate-700/10'>
+				<div className='flex flex-row space-x-2 py-2 w-full px-2'>
+					<FileTile
+						icon={<FolderIcon />}
+						text={"From file"}
+						onFileUpdate={(decoded, blobUrl, mimeType) => {
+							props.transcriber.onInputChange();
+							setAudioData({
+								buffer: decoded,
+								url: blobUrl,
+								source: AudioSource.FILE,
+								mimeType: mimeType,
+							});
+						}}
+					/>
+					<VerticalBar />
+					<LiveTile
+						icon={<FolderIcon />}
+						text={"From Live Stream"}
+						onAudioUpdate={(audioBlob, blobUrl, mimeType, audioBuffer) => {
+							props.transcriber.onInputChange();
+							setAudioData({
+								url: blobUrl,
+								source: AudioSource.LIVE_STREAM,
+								mimeType: mimeType,
+								buffer: audioBuffer
+							});
+						}}
+					/>
+				</div>
+			</div>
+			{audioData && (
+				<>
+					<div className='relative w-full flex justify-center items-center'>
+						<TranscribeButton
+							onClick={() => {
+								props.transcriber.start(audioData.buffer);
+							}}
+							isModelLoading={props.transcriber.isModelLoading}
+							// isAudioLoading ||
+							isTranscribing={props.transcriber.isBusy}
+						/>
+
+						<SettingsTile
+							className='absolute right-4'
+							transcriber={props.transcriber}
+							icon={<SettingsIcon />}
+						/>
+					</div>
+					{props.transcriber.progressItems.length > 0 && (
+						<div className='relative z-10 p-4 w-full'>
+							<label>
+								Loading model files... (only run once)
+							</label>
+							{props.transcriber.progressItems.map((data) => (
+								<div key={data.file}>
+								</div>
+							))}
+						</div>
+					)}
+				</>
+			)}
+		</>
+	);
+}
+
+function SettingsTile(props: {
+	icon: JSX.Element;
+	className?: string;
+	transcriber: Transcriber;
+}) {
+	const [showModal, setShowModal] = useState(false);
+
+	const onClick = () => {
+		setShowModal(true);
+	};
+
+	const onClose = () => {
+		setShowModal(false);
+	};
+
+	const onSubmit = (url: string) => {
+		onClose();
+	};
+
+	return (
+		<div className={props.className}>
+			<Tile icon={props.icon} onClick={onClick} />
+			<SettingsModal
+				show={showModal}
+				onSubmit={onSubmit}
+				onClose={onClose}
+				transcriber={props.transcriber}
+			/>
+		</div>
+	);
+}
+
+function SettingsModal(props: {
+	show: boolean;
+	onSubmit: (url: string) => void;
+	onClose: () => void;
+	transcriber: Transcriber;
+}) {
+	const names = Object.values(LANGUAGES).map(titleCase);
+
+	const models = {
+		tiny: [61, 231],
+		base: [103, 398],
+		small: [290],
+		medium: [833],
+	};
+	return (
+		<Modal
+			show={props.show}
+			title={"Settings"}
+			content={
+				<>
+					<label>Select the model to use.</label>
+					<select
+						className='mt-1 mb-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+						defaultValue={props.transcriber.model}
+						onChange={(e) => {
+							props.transcriber.setModel(e.target.value);
+						}}
+					>
+						{Object.keys(models)
+							.filter(
+								(key) =>
+									props.transcriber.quantized ||
+									// @ts-ignore
+									models[key].length == 2,
+							)
+							.map((key) => (
+								<option key={key} value={key}>{`whisper-${key}${props.transcriber.multilingual ? "" : ".en"
+									} (${
+									// @ts-ignore
+									models[key][
+									props.transcriber.quantized ? 0 : 1
+									]
+									}MB)`}</option>
+							))}
+					</select>
+					<div className='flex justify-between items-center mb-3 px-1'>
+						<div className='flex'>
+							<input
+								id='multilingual'
+								type='checkbox'
+								checked={props.transcriber.multilingual}
+								onChange={(e) => {
+									props.transcriber.setMultilingual(
+										e.target.checked,
+									);
+								}}
+							></input>
+							<label htmlFor={"multilingual"} className='ms-1'>
+								Multilingual
+							</label>
+						</div>
+						<div className='flex'>
+							<input
+								id='quantize'
+								type='checkbox'
+								checked={props.transcriber.quantized}
+								onChange={(e) => {
+									props.transcriber.setQuantized(
+										e.target.checked,
+									);
+								}}
+							></input>
+							<label htmlFor={"quantize"} className='ms-1'>
+								Quantized
+							</label>
+						</div>
+					</div>
+					{props.transcriber.multilingual && (
+						<>
+							<label>Select the source language.</label>
+							<select
+								className='mt-1 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+								defaultValue={props.transcriber.language}
+								onChange={(e) => {
+									props.transcriber.setLanguage(
+										e.target.value,
+									);
+								}}
+							>
+								{Object.keys(LANGUAGES).map((key, i) => (
+									<option key={key} value={key}>
+										{names[i]}
+									</option>
+								))}
+							</select>
+							<label>Select the task to perform.</label>
+							<select
+								className='mt-1 mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+								defaultValue={props.transcriber.subtask}
+								onChange={(e) => {
+									props.transcriber.setSubtask(
+										e.target.value,
+									);
+								}}
+							>
+								<option value={"transcribe"}>Transcribe</option>
+								<option value={"translate"}>
+									Translate (to English)
+								</option>
+							</select>
+						</>
+					)}
+				</>
+			}
+			onClose={props.onClose}
+			onSubmit={() => { }}
+		/>
+	);
+}
+
+function VerticalBar() {
+	return <div className='w-[1px] bg-slate-200'></div>;
+}
+
+function FileTile(props: {
+	icon: JSX.Element;
+	text: string;
+	onFileUpdate: (
+		decoded: AudioBuffer,
+		blobUrl: string,
+		mimeType: string,
+	) => void;
+}) {
+	// const audioPlayer = useRef<HTMLAudioElement>(null);
+
+	// Create hidden input element
+	console.log("1");
+	let elem = document.createElement("input");
+	elem.type = "file";
+	elem.oninput = (event) => {
+		// Make sure we have files to use
+		let files = (event.target as HTMLInputElement).files;
+		if (!files) return;
+
+		// Create a blob that we can use as an src for our audio element
+		const urlObj = URL.createObjectURL(files[0]);
+		const mimeType = files[0].type;
+
+		const reader = new FileReader();
+		console.log("2");
+		reader.addEventListener("load", async (e) => {
+			const arrayBuffer = e.target?.result as ArrayBuffer; // Get the ArrayBuffer
+			if (!arrayBuffer) return;
+
+			const audioCTX = new AudioContext({
+				sampleRate: Constants.SAMPLING_RATE,
+			});
+
+			const decoded = await audioCTX.decodeAudioData(arrayBuffer);
+
+			props.onFileUpdate(decoded, urlObj, mimeType);
+			console.log("3");
+
+		});
+		reader.readAsArrayBuffer(files[0]);
+
+		// Reset files
+		elem.value = "";
+	};
+
+	return (
+		<>
+			<Tile
+				icon={props.icon}
+				text={props.text}
+				onClick={() => elem.click()}
+			/>
+		</>
+	);
+}
+
+function LiveTile(props: {
+	icon: JSX.Element;
+	text: string;
+	onAudioUpdate: (
+		audioBlob: Blob,
+		blobUrl: string,
+		mimeType: string,
+		audioBuffer: AudioBuffer // Add the audio buffer here
+	) => void;
+	// transcriber: Transcriber;
+}) {
+	const handleLiveStreamButtonClick = async () => {
+		try {
+			chrome.tabCapture.capture({ audio: true }, async (stream) => {
+				if (chrome.runtime.lastError) {
+					console.error("Error accessing tab audio 1:", chrome.runtime.lastError);
+					return;
+				}
+
+				if (stream === null) {
+					console.error("Unable to capture tab audio.");
+					return;
+				}
+
+				const audioChunks: Blob[] = [];
+
+				const mediaRecorder = new MediaRecorder(stream);
+				mediaRecorder.addEventListener("dataavailable", (event) => {
+					audioChunks.push(event.data);
+				});
+
+				mediaRecorder.addEventListener("stop", async () => {
+					console.log("stop");
+					const audioBlob = new Blob(audioChunks, { type: "audio/webm" });
+					const blobUrl = URL.createObjectURL(audioBlob);
+
+					// Decode the audio data to get the audio buffer
+					const arrayBuffer = await audioBlob.arrayBuffer();
+					const audioCTX = new AudioContext({ sampleRate: Constants.SAMPLING_RATE });
+					const audioBuffer = await audioCTX.decodeAudioData(arrayBuffer);
+
+					// Pass the audio buffer to the onAudioUpdate function
+					props.onAudioUpdate(audioBlob, blobUrl, "audio/webm", audioBuffer);
+				});
+
+				mediaRecorder.start();
+
+				// Stop the recording after 5 seconds
+				setTimeout(() => {
+					mediaRecorder.stop();
+				}, 5000);
+			});
+		} catch (error) {
+			console.error("Error accessing tab audio 2:", error);
+		}
+	};
+
+	return (
+		<Tile icon={props.icon} text={props.text} onClick={handleLiveStreamButtonClick} />
+	);
+}
+
+function Tile(props: {
+	icon: JSX.Element;
+	text?: string;
+	onClick?: () => void;
+}) {
+	return (
+		<button
+			onClick={props.onClick}
+			className='flex items-center justify-center rounded-lg p-2 bg-blue text-slate-500 hover:text-indigo-600 hover:bg-indigo-50 transition-all duration-200'
+		>
+			<div className='w-7 h-7'>{props.icon}</div>
+			{props.text && (
+				<div className='ml-2 break-text text-center text-md w-30'>
+					{props.text}
+				</div>
+			)}
+		</button>
+	);
+}
+
+function AnchorIcon() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			fill='none'
+			viewBox='0 0 24 24'
+			strokeWidth='1.5'
+			stroke='currentColor'
+		>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244'
+			/>
+		</svg>
+	);
+}
+
+function FolderIcon() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			fill='none'
+			viewBox='0 0 24 24'
+			strokeWidth='1.5'
+			stroke='currentColor'
+		>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M3.75 9.776c.112-.017.227-.026.344-.026h15.812c.117 0 .232.009.344.026m-16.5 0a2.25 2.25 0 00-1.883 2.542l.857 6a2.25 2.25 0 002.227 1.932H19.05a2.25 2.25 0 002.227-1.932l.857-6a2.25 2.25 0 00-1.883-2.542m-16.5 0V6A2.25 2.25 0 016 3.75h3.879a1.5 1.5 0 011.06.44l2.122 2.12a1.5 1.5 0 001.06.44H18A2.25 2.25 0 0120.25 9v.776'
+			/>
+		</svg>
+	);
+}
+
+function SettingsIcon() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			fill='none'
+			viewBox='0 0 24 24'
+			strokeWidth='1.25'
+			stroke='currentColor'
+		>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z'
+			/>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+			/>
+		</svg>
+	);
+}
+
+function MicrophoneIcon() {
+	return (
+		<svg
+			xmlns='http://www.w3.org/2000/svg'
+			fill='none'
+			viewBox='0 0 24 24'
+			strokeWidth={1.5}
+			stroke='currentColor'
+		>
+			<path
+				strokeLinecap='round'
+				strokeLinejoin='round'
+				d='M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z'
+			/>
+		</svg>
+	);
+}
