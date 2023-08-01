a88db769aedc898d85abd2c3d33dcb9b1f45b560
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-08-01 21:26:06 +0930

    chore: Remove Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 export default function App() {
     return (
-        <div className='flex justify-center items-center min-h-screen'>
-            <div className='container flex flex-col justify-center items-center'>
-                <h1 className='text-3xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
-                    Lecture Master
-                </h1>
-                <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
+        <div className="app-container">
+            <div className="container flex-container">
+                <h1 className="heading-1">Lecture Master</h1>
+                <h2 className="heading-2">
                     Makes watching lectures more efficient!
                 </h2>
             </div>
 
-            <div className='absolute bottom-4'>
+            <div className="developer-info">
                 Developed by{" "}
                 <a
-                    className='underline'
-                    href='https://github.com/phoenixpereira'
-                    target="blank"
+                    className="developer-link"
+                    href="https://github.com/phoenixpereira"
+                    target="_blank"
+                    rel="noopener noreferrer"
                 >
                     Phoenix Pereira
                 </a>
             </div>
-
         </div>
     );
 }
