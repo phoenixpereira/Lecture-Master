1d0b1669c8800e4c442baa3f6c4df46097a69f60
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-08-04 10:11:53 +0930

    feat: Readded silence skipping

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 export default function App() {
     return (
-        <div className='flex justify-center items-center min-h-screen'>
-            <div className='container flex flex-col justify-center items-center'>
-                <h1 className='text-3xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center'>
+        <div className="flex justify-center items-center min-h-screen">
+            <div className="container flex flex-col justify-center items-center">
+                <h1 className="text-3xl font-extrabold tracking-tight text-slate-900 sm:text-7xl text-center">
                     Lecture Master
                 </h1>
-                <h2 className='mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl'>
+                <h2 className="mt-3 mb-5 px-4 text-center text-1xl font-semibold tracking-tight text-slate-900 sm:text-2xl">
                     Makes watching lectures more efficient!
                 </h2>
             </div>
 
-            <div className='absolute bottom-4'>
+            <div className="absolute bottom-4">
                 Developed by{" "}
                 <a
-                    className='underline'
-                    href='https://github.com/phoenixpereira'
+                    className="underline"
+                    href="https://github.com/phoenixpereira"
                     target="blank"
                 >
                     Phoenix Pereira
                 </a>
             </div>
-
         </div>
     );
 }
---
+++ src/contentScript.ts
@@ -0,0 +1,148 @@
+// Initialise variables
+let audioContext: AudioContext | undefined;
+let source: MediaElementAudioSourceNode | undefined;
+let analyser: AnalyserNode | undefined;
+let gainNode: GainNode | undefined;
+let isPausedOrEnded: boolean = false;
+let currentPlaybackRate: number = 1;
+let targetPlaybackRate: number = 1;
+let isTransitioning: boolean = false;
+
+// Transition settings
+let transitionDuration: number = 0.25; // Duration of the transition in seconds
+let transitionInterval: number = 10; // Interval between transition steps in milliseconds
+
+// Silence detection settings
+let silenceCounter: number = 0;
+let silenceThreshold: number = -5.9; // Threshold for silence in decibels
+let requiredSilenceFrames: number = 3; // Number of frames required for silence
+
+// Create the audio context and gain node
+function createAudioContext() {
+    audioContext = new window.AudioContext();
+    gainNode = audioContext.createGain();
+}
+
+// Get the video volume and adjust playback rate
+function getVideoVolume(video: HTMLMediaElement) {
+    if (!audioContext) {
+        createAudioContext();
+    }
+
+    if (!source || !analyser) {
+        source = audioContext!.createMediaElementSource(video);
+        analyser = audioContext!.createAnalyser();
+        source.connect(analyser);
+        analyser.connect(gainNode!);
+        gainNode!.connect(audioContext!.destination);
+    }
+
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    const maxAmplitude = Math.max(...Array.from(dataArray));
+    const volumeInDecibels = 20 * Math.log10(maxAmplitude / 255);
+    const threshold = -5.9;
+    console.log(targetPlaybackRate);
+
+    if (volumeInDecibels >= silenceThreshold) {
+        silenceCounter = 0;
+        setTargetPlaybackRate(1);
+        console.log("1x");
+    } else {
+        if (silenceCounter >= requiredSilenceFrames) {
+            setTargetPlaybackRate(2);
+            console.log("2x");
+        } else {
+            setTargetPlaybackRate(1);
+            console.log("1x");
+        }
+        silenceCounter++;
+    }
+
+    if (!isPausedOrEnded) {
+        setTimeout(() => {
+            getVideoVolume(video);
+        }, 200); // Run the function again after 200 milliseconds
+    }
+}
+
+// Set the target playback rate
+function setTargetPlaybackRate(rate: number) {
+    targetPlaybackRate = Math.max(0.5, Math.min(2, rate)); // Limit target rate to supported range (0.5 to 2)
+    if (isTransitioning) return; // Ignore if transition is already in progress
+    startPlaybackTransition();
+}
+
+// Perform the playback rate transition
+function startPlaybackTransition() {
+    isTransitioning = true;
+    const totalSteps = Math.ceil(
+        (transitionDuration * 1000) / transitionInterval
+    );
+    const playbackRateStep =
+        (targetPlaybackRate - currentPlaybackRate) / totalSteps;
+    let stepCount = 0;
+
+    function updatePlaybackRate() {
+        stepCount++;
+        currentPlaybackRate += playbackRateStep;
+
+        if (stepCount >= totalSteps) {
+            currentPlaybackRate = targetPlaybackRate;
+            isTransitioning = false;
+            videoElement!.playbackRate = currentPlaybackRate;
+            return;
+        }
+
+        videoElement!.playbackRate = currentPlaybackRate;
+        setTimeout(updatePlaybackRate, transitionInterval);
+    }
+
+    updatePlaybackRate();
+}
+
+// Handle video events (play, pause, ended)
+function handleVideoEvents(event: Event) {
+    const video = event.target as HTMLMediaElement;
+
+    if (video.paused || video.ended) {
+        if (audioContext) {
+            audioContext.suspend();
+        }
+        isPausedOrEnded = true;
+    } else {
+        if (!audioContext) {
+            createAudioContext();
+        } else {
+            audioContext.resume();
+            isPausedOrEnded = false;
+            getVideoVolume(video);
+        }
+    }
+}
+
+// Find the first video element on the page
+let videoElement: HTMLVideoElement | null = null;
+
+// Loops through the mutations in the mutations list and finds video element
+const observer = new MutationObserver((mutationsList) => {
+    for (const mutation of mutationsList) {
+        if (mutation.type === "childList") {
+            videoElement = document.querySelector("video");
+
+            if (videoElement) {
+                observer.disconnect();
+                videoElement.addEventListener("play", handleVideoEvents);
+                videoElement.addEventListener("pause", handleVideoEvents);
+                videoElement.addEventListener("ended", handleVideoEvents);
+                break;
+            }
+        }
+    }
+});
+
+// Start observing changes in the body element
+observer.observe(document.body, { childList: true, subtree: true });
