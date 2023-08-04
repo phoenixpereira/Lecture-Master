2f1333b7f24b77dea3a78d923a2bc961e502eabe
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-08-04 17:36:59 +0930

    build: Combined build commands

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,16 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		react()
-	],
-	build: {
-		outDir: path.join(__dirname, "build"),
-		emptyOutDir: true,
-		rollupOptions: {
-			output: {
-				entryFileNames: "assets/[name].js",
-				chunkFileNames: "assets/[name].js",
-				assetFileNames: "assets/[name].[ext]"
-			},
-		}
-	},
+    plugins: [react()],
+    build: {
+        outDir: path.join(__dirname, "build"),
+        emptyOutDir: true,
+        rollupOptions: {
+            output: {
+                entryFileNames: "assets/[name].js",
+                chunkFileNames: "assets/[name].js",
+                assetFileNames: "assets/[name].[ext]"
+            }
+        }
+    }
 });
-
