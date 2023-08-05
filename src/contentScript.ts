819609c3e257472c190f5c0b1bc6ad36e6afa22a
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2023-08-05 21:47:33 +0930

    chore: Add check for analyser type

--- src/contentScript.ts
+++ src/contentScript.ts
@@ -2,7 +2,10 @@
 
 // Initialise variables
 let audioContext: AudioContext | undefined;
-let source: MediaElementAudioSourceNode | undefined;
+let source:
+    | MediaElementAudioSourceNode
+    | MediaStreamAudioSourceNode
+    | undefined;
 let analyser: AnalyserNode | undefined;
 let gainNode: GainNode | undefined;
 let isPausedOrEnded: boolean = false;
@@ -33,84 +36,149 @@ function createAudioContext() {
 // Get the video volume and adjust playback rate
 function getVideoVolume(video: HTMLMediaElement) {
     chrome.storage.local.get(
-        ["normalPlaybackRate", "silentPlaybackRate", "silenceThreshold"],
+        [
+            "normalPlaybackRate",
+            "silentPlaybackRate",
+            "silenceThreshold",
+            "analyserType"
+        ],
         (data) => {
             normalPlaybackRate = data.normalPlaybackRate || 1;
             silentPlaybackRate = data.silentPlaybackRate || 1;
             silenceThreshold = data.silenceThreshold || -5.9;
-            targetPlaybackRate = normalPlaybackRate;
+            const analyserType = data.analyserType || "tabOutputAnalyser";
+
+            if (analyserType === "tabOutputAnalyser") {
+                chrome.tabCapture.capture({ audio: true }, (stream) => {
+                    // Create an AudioContext from the captured audio stream
+                    audioContext = new AudioContext();
+                    source = audioContext.createMediaStreamSource(
+                        stream as MediaStream
+                    );
+                    analyser = audioContext.createAnalyser();
+                    source.connect(analyser);
+                    analyser.connect(gainNode!);
+                    gainNode!.connect(audioContext!.destination);
+                    const bufferLength = analyser.frequencyBinCount;
+                    const dataArray = new Uint8Array(bufferLength);
+
+                    analyser.getByteFrequencyData(dataArray);
+
+                    // Calculate the average amplitude value
+                    let sumAmplitude = dataArray.reduce(
+                        (acc, value) => acc + value,
+                        0
+                    );
+                    const averageAmplitude = sumAmplitude / dataArray.length;
+
+                    // Normalise the amplitude to a value between 0 and 1
+                    const normalizedAmplitude = averageAmplitude / 255;
+
+                    // Calculate the volume in decibels using the normalized amplitude
+                    const volumeInDecibels =
+                        10 * Math.log10(normalizedAmplitude);
+                    console.log(
+                        "Actual Volume: ",
+                        volumeInDecibels,
+                        "Threshold: ",
+                        silenceThreshold,
+                        "Speed: ",
+                        videoElement!.playbackRate
+                    );
+
+                    if (volumeInDecibels >= silenceThreshold) {
+                        silenceCounter = 0;
+                        videoElement!.playbackRate = normalPlaybackRate;
+                    } else {
+                        if (silenceCounter >= requiredSilenceFrames) {
+                            videoElement!.playbackRate = silentPlaybackRate;
+                        } else {
+                            videoElement!.playbackRate = normalPlaybackRate;
+                        }
+                        silenceCounter++;
+                    }
+
+                    if (!isPausedOrEnded) {
+                        setTimeout(() => {
+                            getVideoVolume(video);
+                        }, 200); // Run the function again after 200 milliseconds
+                    }
+                });
+            } else if (analyserType === "elementAnalyser") {
+                if (!audioContextInitialised) {
+                    createAudioContext();
+                    audioContextInitialised = true;
+                }
+                if (!source || !analyser) {
+                    source = audioContext!.createMediaElementSource(video);
+                    analyser = audioContext!.createAnalyser();
+                    source.connect(analyser);
+                    analyser.connect(gainNode!);
+                    gainNode!.connect(audioContext!.destination);
+                }
+                const bufferLength = analyser.frequencyBinCount;
+                const dataArray = new Uint8Array(bufferLength);
+
+                analyser.getByteFrequencyData(dataArray);
+
+                // Calculate the average amplitude value
+                let sumAmplitude = dataArray.reduce(
+                    (acc, value) => acc + value,
+                    0
+                );
+                const averageAmplitude = sumAmplitude / dataArray.length;
+
+                // Normalise the amplitude to a value between 0 and 1
+                const normalizedAmplitude = averageAmplitude / 255;
+
+                // Calculate the volume in decibels using the normalized amplitude
+                const volumeInDecibels = 10 * Math.log10(normalizedAmplitude);
+                console.log(
+                    "Actual Volume: ",
+                    volumeInDecibels,
+                    "Threshold: ",
+                    silenceThreshold,
+                    "Speed: ",
+                    videoElement!.playbackRate
+                );
+
+                if (volumeInDecibels >= silenceThreshold) {
+                    silenceCounter = 0;
+                    videoElement!.playbackRate = normalPlaybackRate;
+                } else {
+                    if (silenceCounter >= requiredSilenceFrames) {
+                        videoElement!.playbackRate = silentPlaybackRate;
+                    } else {
+                        videoElement!.playbackRate = normalPlaybackRate;
+                    }
+                    silenceCounter++;
+                }
+
+                if (!isPausedOrEnded) {
+                    setTimeout(() => {
+                        getVideoVolume(video);
+                    }, 200); // Run the function again after 200 milliseconds
+                }
+            }
         }
     );
-
-    if (!audioContextInitialised) {
-        createAudioContext();
-        audioContextInitialised = true;
-    }
-
-    if (!source || !analyser) {
-        source = audioContext!.createMediaElementSource(video);
-        analyser = audioContext!.createAnalyser();
-        source.connect(analyser);
-        analyser.connect(gainNode!);
-        gainNode!.connect(audioContext!.destination);
-    }
-
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-
-    analyser.getByteFrequencyData(dataArray);
-
-    // Calculate the average amplitude value
-    let sumAmplitude = dataArray.reduce((acc, value) => acc + value, 0);
-    const averageAmplitude = sumAmplitude / dataArray.length;
-
-    // Normalise the amplitude to a value between 0 and 1
-    const normalizedAmplitude = averageAmplitude / 255;
-
-    // Calculate the volume in decibels using the normalized amplitude
-    const volumeInDecibels = 10 * Math.log10(normalizedAmplitude);
-    console.log(
-        "Actual Volume: ",
-        volumeInDecibels,
-        "Threshold: ",
-        silenceThreshold,
-        "Speed: ",
-        videoElement!.playbackRate
-    );
-
-    if (volumeInDecibels >= silenceThreshold) {
-        silenceCounter = 0;
-        videoElement!.playbackRate = normalPlaybackRate;
-    } else {
-        if (silenceCounter >= requiredSilenceFrames) {
-            videoElement!.playbackRate = silentPlaybackRate;
-        } else {
-            videoElement!.playbackRate = normalPlaybackRate;
-        }
-        silenceCounter++;
-    }
-
-    if (!isPausedOrEnded) {
-        setTimeout(() => {
-            getVideoVolume(video);
-        }, 200); // Run the function again after 200 milliseconds
-    }
 }
 
-// Handle video events (play, pause, ended)
-function handleVideoEvents(event: Event) {
-    const video = event.target as HTMLMediaElement;
+function handleVideoEvents(analyserType: string) {
+    return function (event: Event) {
+        const video = event.target as HTMLMediaElement;
 
-    if (video.paused || video.ended) {
-        isPausedOrEnded = true;
-    } else {
-        if (!audioContextInitialised) {
-            createAudioContext();
-            audioContextInitialised = true;
+        if (video.paused || video.ended) {
+            isPausedOrEnded = true;
+        } else {
+            if (!audioContextInitialised) {
+                createAudioContext();
+                audioContextInitialised = true;
+            }
+            isPausedOrEnded = false;
+            getVideoVolume(video);
         }
-        isPausedOrEnded = false;
-        getVideoVolume(video);
-    }
+    };
 }
 
 // Find the first video element on the page
@@ -124,9 +192,41 @@ const observer = new MutationObserver((mutationsList) => {
 
             if (videoElement) {
                 observer.disconnect();
-                videoElement.addEventListener("play", handleVideoEvents);
-                videoElement.addEventListener("pause", handleVideoEvents);
-                videoElement.addEventListener("ended", handleVideoEvents);
+                // Get the analyserType value from chrome.storage.local
+                chrome.storage.local.get(["analyserType"], (data) => {
+                    const analyserType =
+                        data.analyserType || "tabOutputAnalyser"; // Use the default value if not found
+                    if (analyserType === "tabOutputAnalyser") {
+                        // No need to listen to video events for 'tabOutputAnalyser'
+                        if (videoElement) {
+                            videoElement.removeEventListener(
+                                "play",
+                                handleVideoEvents(analyserType)
+                            );
+                            videoElement.removeEventListener(
+                                "pause",
+                                handleVideoEvents(analyserType)
+                            );
+                            videoElement.removeEventListener(
+                                "ended",
+                                handleVideoEvents(analyserType)
+                            );
+                        }
+                    } else if (analyserType === "elementAnalyser") {
+                        // Use observer for 'elementAnalyser'
+                        observer.observe(document.body, {
+                            childList: true,
+                            subtree: true
+                        });
+                    }
+                    const eventHandler = handleVideoEvents(analyserType);
+                    if (videoElement) {
+                        videoElement.addEventListener("play", eventHandler);
+                        videoElement.addEventListener("pause", eventHandler);
+                        videoElement.addEventListener("ended", eventHandler);
+                    }
+                });
+
                 break;
             }
         }
